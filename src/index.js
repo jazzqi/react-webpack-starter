6886652513541a3cd17d5f0c9e3af9e684e61c7f
qfei <jazzqi@gmail.com>
2018-09-14 13:36:45 +0800

    messed up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import Redux from 'redux';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
+// create redux reducers
+const reducer = Redux.combineReducers({
+  todos: (state = [], action) => {
+    // use immutable.js
+    const newState = Object.assign([], state);
+    if (action.type == 'add') {
+      newState.push(action.items)
+    }
+    // return ['grocerires', 'make dinner'];
+    return newState;
+  }
+})
+
+// use that reducer to create a store
+const store = Redux.createStore(reducer);
+
+const state = store.getState();
+
+state.todos.forEach((s) => {
+  console.log(s)
+});
+
+document.getElementById('submit-button').onclick = () => {
+  store.dispatch({
+    type: 'add', // type based on action
+    item: document.getElementById('todo').value;
+  });
+  // render();
+};
