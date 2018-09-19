5052fb3b88c7688bb2197f507e5d8ab129ea9ed0
qfei <jazzqi@gmail.com>
2018-09-20 01:14:40 +0800

    more life cycle events

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import Counter from './Counter'
+import React, { Component } from "react";
+import Counter from "./Counter";
 
 // React let us define components as classes or as functions.
 
@@ -8,11 +8,60 @@ import Counter from './Counter'
 // 2. updating
 // 3. error boundaries
 // 4. unmounting
-class App extends Component { // or extends React.components
+class App extends Component {
+  // or extends React.components
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mount: true,
+      ignoreProp: 0,
+      seed: 60,
+      showErrorComponent: false
+    };
+
+    this.mountCounter = () => {
+      this.setState({ mount: true });
+    };
+    this.unmountCounter = () => {
+      this.setState({ mount: false });
+    };
+
+    this.ignoreProp = () => {
+      this.setState({ ignoreProp: Math.random() });
+    };
+
+    this.seedGenerator = () => {
+      this.setState({
+        seed: Number.parseInt(Math.random() * 100)
+      });
+    };
+
+    this.toggleError = () =>
+      this.setState({ showErrorComponent: !this.showErrorComponent });
+  }
+
   render() {
-    return <div>
-      <Counter></Counter>
-    </div>
+    return (
+      <div>
+        <button onClick={this.mountCounter} disabled={this.state.mount}>
+          Mount Counter
+        </button>
+        <button onClick={this.unmountCounter} disabled={!this.state.mount}>
+          Unmount Counter
+        </button>
+        <button onClick={this.ignoreProp}>Ignore Prop</button>
+        <button onClick={this.seedGenerator}>Generate Seed</button>
+        <button onClick={this.toggleError}>Toggle Error</button>
+        {this.state.mount ? (
+          <Counter
+            ignoreProp={this.state.ignoreProp}
+            seed={this.state.seed}
+            showErrorComponent={this.state.showErrorComponent}
+          />
+        ) : null}
+      </div>
+    );
   }
 }
 
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+const ErrorComponent = () => <div>{props.ignore}</div>;
+
 export default class Counter extends Component {
   constructor(props) {
     // Constructor 是唯一初始化 state 的地方
@@ -7,32 +9,103 @@ export default class Counter extends Component {
     super(props);
 
     this.state = {
-      counter: 0
+      counter: 0,
+      seed: 0,
+      initializing: true
     };
 
     this.increment = () => this.setState({ counter: this.state.counter + 1 });
     this.decrement = () => this.setState({ counter: this.state.counter - 1 });
   }
 
+  // Called before anyother methods
+  // The purpose of this method is to
+  // give you a chance to copy any values
+  // from props that you may be interested in
+  // transferring over to state
+  // COPY props INTO state
+  // 静态方法，无法访问实例本身
+  static getDerivedStateFromProps(props, state) {
+    if (props.seed && state.seed !== props.seed) {
+      return {
+        seed: props.seed,
+        counter: props.seed
+      };
+    }
+
+    return null;
+    // any thing rather than null returned here will be the state
+  }
+
   componentDidMount() {
     console.log("Component Did Mount");
+    setTimeout(() => {
+      this.setState({
+        initializing: false
+      });
+    }, 1000);
     console.log("-------------------");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextProps.ignoreProp &&
+      this.props.ignoreProp !== nextProps.ignoreProp
+    ) {
+      console.log("Should Component Update - DO NOT RENDER");
+      return false;
+    } else {
+      console.log("Should Component Update - RENDER");
+      return true;
+    }
   }
 
   render() {
     console.log("Render");
+
+    if (this.state.initializing) {
+      return <div>Initializing</div>;
+    }
+
+    if (this.props.showErrorComponent && this.state.error) {
+      return <div>We have encountered an error! {this.state.error}</div>;
+    }
 
     return (
       <div>
         <button onClick={this.increment}>Increment</button>
         <button onClick={this.decrement}>Decrement</button>
         <div className="counter">Counter: {this.state.counter}</div>
+        {this.props.showErrorComponent ? <ErrorComponent /> : null}
       </div>
     );
+  }
+
+  // Capture some properties that are not stored
+  // in the state before we rerender that component
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("Get Snapshot Before Update");
+    return null;
+    // 这里返回的对象，将是 componentWillMount() 的第三个参数
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     console.log("Component Did Update");
     console.log("-------------------");
+  }
+
+  componentWillUnmount() {
+    console.log("Component Will Umount");
+    console.log("-------------------");
+  }
+
+  // if not catch error, the app will crash
+  componentDidCatch(error, info) {
+    console.log("Component Did Catch", error, info);
+
+    this.setState({
+      error,
+      info
+    });
   }
 }
